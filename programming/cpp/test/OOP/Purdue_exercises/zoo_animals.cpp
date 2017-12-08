08ad48fa9d9d906c387c1f751d45375fce268ac4
Dave Campbell <djcampbel1914@gmail.com>
2017-12-08 09:47:02 -0800

    Added a bit more code from Purdue exercises

--- programming/cpp/test/OOP/Purdue_exercises/zoo_animals.cpp
+++ programming/cpp/test/OOP/Purdue_exercises/zoo_animals.cpp
@@ -4,16 +4,33 @@ enum scale {ounces, kilograms};
 
 class ZooAnimal  
 {
+public:
+  class nutrition
+  {
+  private:
+    int numberMeals;
+    int maxPoundsFed;
+    float avgNutrValue;
+  public:
+    inline nutrition () {
+      numberMeals = 4;
+      maxPoundsFed = 75;
+      avgNutrValue = 38.21;};
+    inline float reptMealNutrition () 
+    {return numberMeals * avgNutrValue;};
+  };
 private:
   const char *name;
   int cageNumber;
   int weightDate;
   int weight;
   ZooAnimal *mother;
+  nutrition nutr;
 public:
   ZooAnimal(const char*, int, int, int);
   inline ~ZooAnimal();
-  
+
+  void changeOldestWeightDate(int date);
   void changeWeight (int pounds);
   void changeWeightDate (int today);
   int reptWeight ();
@@ -25,9 +42,46 @@ public:
   void isMotherOf (ZooAnimal&);
   // This is kinda a weird one. Basically how you can call this is by
   // explicitly calling "int(ZooAnimal)" or "(int)ZooAnimal" or it can
-  // be automatically converted in an operation like this: "int + ZooAnimal"
+  // be automatically converted in an operation like this: "int +
+  // ZooAnimal"
+
   operator int();
+
+  // Statc members require a bit of explanation.
+  //
+  // Basiclly, there is only one value for static member variables
+  // that persist throughout the scope of the class. As opposed to
+  // "normal" class members that you can change with each instance of
+  // the class. For instance, you can have an object counter that you
+  // can initialize at 0 then incrememnt every time an instance is
+  // created (you would do that in the constructor).
+  //
+  // In order to access and therefore even use the static member, you
+  // have to declare it (that is done oustside the class - you can do
+  // that right below the definition of the class like I've done in
+  // this file...int ZooAnimal::oldestWeightDate; ) Once you do that,
+  // any member function you define that tries to access it can,
+  // static or otherwise.
+  //
+  // See
+  //  https://www.tutorialspoint.com/cplusplus/cpp_static_members.htm
+  //  for more info.
+  
+  static int oldestWeightDate;
+  float reptNutrition();
 };
+
+float ZooAnimal::reptNutrition()
+{
+  nutr.reptMealNutrition();
+}
+
+int ZooAnimal::oldestWeightDate;
+
+void ZooAnimal::changeOldestWeightDate(int date)
+{
+  oldestWeightDate = date;
+}
 
 ZooAnimal::~ZooAnimal()
 {
@@ -90,16 +144,18 @@ int ZooAnimal::daysSinceLastWeighed(int today)
   return (thisday-startday);
 }
 
+
+int reptOldestZooAnimalWeightDate()
+{
+  return ZooAnimal::oldestWeightDate;
+}
+
 int main ()
 {
   const char* name = "Bozo";
   ZooAnimal bozo(name, 408, 1027, 400);
 
   bozo.reptName();
-  //std::cout << "This animal's name is " << bozo.reptName() << std::endl;
-
-  // For some reason, this function does some crazy stuff. I'm going
-  // to keep it commented out for now
-  // bozo.Destroy ();
+  std::cout << int(bozo) << std::endl;
 }
 
