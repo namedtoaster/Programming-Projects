9ab62a67aeaa3034d82bf15a945e147494530897
Dave Campbell <djcampbel1914@gmail.com>
2017-12-20 23:07:40 -0800

    More exercises

--- programming/cpp/test/OOP/Purdue_exercises/zoo_animals.cpp
+++ programming/cpp/test/OOP/Purdue_exercises/zoo_animals.cpp
@@ -2,32 +2,18 @@
 
 enum scale {ounces, kilograms};
 
+
 class ZooAnimal  
 {
+  // The keywords 'friend' and 'class' (in that order) before the
+  // class name 'cageAssignment' designates it as a friend of the
+  // ZooAnimal class. In this case, cageAssignment is able to access
+  // the private and protected values of ZooAnimal
+  friend class cageAssignment;
+  
 public:
-  class nutrition
-  {
-  private:
-    int numberMeals;
-    int maxPoundsFed;
-    float avgNutrValue;
-  public:
-    inline nutrition () {
-      numberMeals = 4;
-      maxPoundsFed = 75;
-      avgNutrValue = 38.21;};
-    inline float reptMealNutrition () 
-    {return numberMeals * avgNutrValue;};
-  };
-private:
-  const char *name;
-  int cageNumber;
-  int weightDate;
-  int weight;
-  ZooAnimal *mother;
-  nutrition nutr;
-public:
-  ZooAnimal(const char*, int, int, int);
+  ZooAnimal(char*, int, int, int);
+  ZooAnimal(char*);
   inline ~ZooAnimal();
 
   void changeOldestWeightDate(int date);
@@ -36,8 +22,7 @@ public:
   int reptWeight ();
   void reptWeight (scale which);
   inline int reptWeightDate (); 
-  void Destroy (); // destroy function
-  const char* reptName ();
+  char* reptName ();
   int daysSinceLastWeighed (int today);
   void isMotherOf (ZooAnimal&);
   // This is kinda a weird one. Basically how you can call this is by
@@ -47,7 +32,7 @@ public:
 
   operator int();
 
-  // Statc members require a bit of explanation.
+  // Static members require a bit of explanation.
   //
   // Basiclly, there is only one value for static member variables
   // that persist throughout the scope of the class. As opposed to
@@ -69,27 +54,149 @@ public:
   
   static int oldestWeightDate;
   float reptNutrition();
+  
+  class nutrition
+  {
+  private:
+    int numberMeals;
+    int maxPoundsFed;
+    float avgNutrValue;
+  public:
+    inline nutrition () {
+      numberMeals = 4;
+      maxPoundsFed = 75;
+      avgNutrValue = 38.21;};
+    inline float reptMealNutrition () 
+    {return numberMeals * avgNutrValue;};
+  };
+
+  // Friend functions are those that can access the private and public
+  // members of the class they are friends of
+  friend ZooAnimal duplicate(ZooAnimal);
+
+  static ZooAnimal* start;
+  static ZooAnimal* current;
+  static int numAnmls;
+  
+private:
+  char *name;
+  int cageNumber;
+  int weightDate;
+  int weight;
+  ZooAnimal *mother;
+  nutrition nutr;
+  ZooAnimal* prevAnml;
+  ZooAnimal* nextAnml;
+
+  void updateLL();
 };
+
+
+// Previously I declared the cageAssignment class before the ZooAnimal
+// class. I then forward declared the ZooAnimal class - just type
+// 'class ZooAnimal' sometime before the cageAssignment class
+// definition. This threw an error which basically said it didn't have
+// enough info about the ZooAnimal class. Since there isn't really
+// anything in the ZooAnimal class that needs to know about the
+// cageAssignment class, I just put the cageAssignment class below the
+// ZooAnimal class. Problem solved.
+//
+/// See
+/// https://stackoverflow.com/questions/12466055/field-has-incomplete-type-error
+/// for more info
+
+class cageAssignment {
+public:
+    // Test function to test the frienship between cageAssignment and ZooAnimal
+  void test(ZooAnimal a) { value = a.weight; }
+  
+  void changeCage(ZooAnimal&);
+  friend int reptCage(ZooAnimal&);
+private:
+  // A test value to test the friendship between cageAssignment and ZooAnimal
+  int value;
+
+  int enclosure;
+};
+
+
+
+
+
+// Static member initialization
+int ZooAnimal::oldestWeightDate;
+ZooAnimal* ZooAnimal::start = NULL;
+ZooAnimal* ZooAnimal::current = NULL;
+int ZooAnimal::numAnmls = 0;
+
+// Class member functions
+
+void ZooAnimal::updateLL()
+{
+  // Modify linked list
+  if (current == NULL)
+    {
+      start = this;
+      current = this;
+      std::cout << "Start/current are NULL" << std::endl;
+    }
+  else
+    {
+      prevAnml = current;
+      prevAnml->nextAnml = this;
+      nextAnml = NULL;
+      current = this;
+      std::cout << "Start/current are not NULL" << std::endl;
+    }
+
+  // Increase the number of ZooAnimals
+  numAnmls++;
+}
+
+void cageAssignment::changeCage(ZooAnimal& changeMe)
+{
+  changeMe.cageNumber = enclosure;
+  enclosure++;
+}
 
 float ZooAnimal::reptNutrition()
 {
   nutr.reptMealNutrition();
 }
 
-int ZooAnimal::oldestWeightDate;
-
 void ZooAnimal::changeOldestWeightDate(int date)
 {
   oldestWeightDate = date;
 }
 
+ZooAnimal::ZooAnimal(char* n, int a, int b, int c)
+{
+  name = new char;
+
+  name = n;
+  cageNumber = a;
+  weightDate = b;
+  weight = c;
+
+  updateLL();
+}
+
+ZooAnimal::ZooAnimal(char* n)
+{
+  name = new char;
+  
+  name = n;
+
+  updateLL();
+}
+
 ZooAnimal::~ZooAnimal()
 {
-  // Not sure why, but when the destructor gets automatically called,
+  // Not sure why, but when the destructor gets (automatically) called,
   // the compiler complains about an invalid pointer and it spews out
   // the memory map. Look at this in the future
-  //delete name;
-  //delete mother;
+  // delete name;
+  // delete mother;
 }
 
 ZooAnimal::operator int()
@@ -110,23 +217,8 @@ inline int ZooAnimal::reptWeightDate()
   return weightDate;
 }
 
-ZooAnimal::ZooAnimal(const char* n = "Nameless", int a = 9999, int b =
-101, int c = 100)
-{
-  name = n;
-  cageNumber = a;
-  weightDate = b;
-  weight = c;
-}
-
-void ZooAnimal::Destroy ()
-{	
-  delete name;
-  name = NULL;
-}
-
 // -------- member function to return the animal's name
-const char* ZooAnimal::reptName ()
+char* ZooAnimal::reptName ()
 {
   std::cout << name << std::endl;
   return name;
@@ -152,10 +244,12 @@ int reptOldestZooAnimalWeightDate()
 
 int main ()
 {
-  const char* name = "Bozo";
-  ZooAnimal bozo(name, 408, 1027, 400);
+  ZooAnimal bozo("Bozo", 408, 1027, 400);
 
   bozo.reptName();
   std::cout << int(bozo) << std::endl;
+
+  ZooAnimal test("Bill");
+  test.reptName();
 }
 
