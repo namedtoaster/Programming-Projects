f1505dd9a0f57d04371a75211d4e692dfa88c831
Dave Campbell <djcampbell1914@gmail.com>
2018-04-20 16:02:07 -0700

    removed files

---
+++ programming/cpp/open-gl/learnopengldotcom/2-HelloTriangle/build/CMakeFiles/HelloTriangle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloTriangle.dir/source/main.cpp.o"
+  "HelloTriangle.pdb"
+  "HelloTriangle"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloTriangle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
