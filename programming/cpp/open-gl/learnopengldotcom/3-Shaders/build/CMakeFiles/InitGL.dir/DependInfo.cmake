06c78b077236a27130f6ccef0ba53dd2355e826c
Dave Campbell <djcampbell1914@gmail.com>
2018-04-25 12:34:24 -0700

    Add new directory for new tutorial

---
+++ programming/cpp/open-gl/learnopengldotcom/3-Shaders/build/CMakeFiles/InitGL.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dave/Documents/git/programming-math/programming/cpp/open-gl/learnopengldotcom/3-Shaders/source/main.cpp" "/home/dave/Documents/git/programming-math/programming/cpp/open-gl/learnopengldotcom/3-Shaders/build/CMakeFiles/InitGL.dir/source/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../source"
+  "../libraries/glfw/include"
+  "../libraries/glad/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/dave/Documents/git/programming-math/programming/cpp/open-gl/learnopengldotcom/3-Shaders/build/libraries/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/home/dave/Documents/git/programming-math/programming/cpp/open-gl/learnopengldotcom/3-Shaders/build/CMakeFiles/glad.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
