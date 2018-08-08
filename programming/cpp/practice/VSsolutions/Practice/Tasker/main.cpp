7235c4b06dab3da82b2dcbb32b7d9a4d5621bef3
namedtoaster <djcampbell1914@gmail.com>
2018-08-08 12:48:05 -0600

    Complete tasker

--- programming/cpp/practice/VSsolutions/Practice/Tasker/main.cpp
+++ programming/cpp/practice/VSsolutions/Practice/Tasker/main.cpp
@@ -4,28 +4,69 @@
 #include <string>
 #include <ctime>
 
-int main() {
-	// init
-		// load the file with all of the tasks
+const char* tasksFile = "tasks";
+std::vector<std::string> tasks;
+int startSize;
+
+void init() {
+	// load the file with all of the tasks
 	std::ifstream input;
-	input.open("tasks");
-	
-		// put each task into the task vector (just strings)
-	std::vector<std::string> tasks;
+	input.open(tasksFile);
+
+	// put all saved tasks into the vector
 	std::string line;
-	
 	while (getline(input, line)) {
 		tasks.push_back(line);
 	}
 
-	// run
+	// record how big the vector is so you know where to start saving from after you add new tasks
+	startSize = tasks.size();
+}
+
+void printRandTask() {
 	srand(time(NULL));
 
-		// pick a random number between 0 and vector.size
+	// pick a random number between 0 and vector.size
 	int randNum = rand() % tasks.size();
 
-		// display that task
+	// display that task
 	std::cout << tasks[randNum] << std::endl;
+}
+
+void addNewTask() {
+	for (int i = 0; i < tasks.size(); i++) {
+		std::cout << tasks[i] << std::endl;
+	}
+
+	std::cout << "Task: ";
+	std::string input;
+	getline(std::cin, input);
+	tasks.push_back(input);
+}
+
+void saveTasks() {
+	if (tasks.size() > startSize) {
+		std::ofstream output;
+		output.open(tasksFile, std::ios::app);
+
+		for (int i = startSize; i < tasks.size(); i++) {
+			output << '\n' << tasks[i];
+		}
+	}
+}
+
+int main() {
+	// init
+	init();
+
+	// print a task
+	//printRandTask();
+
+	// add a task
+	addNewTask();
+
+	// save new tasks
+	saveTasks();
 
 	return 0;
 }
